7fe9a63e47bdcef617213bb075466fa0eff3b548
jpradocueva <jpradocueva@users.noreply.github.com>
2022-10-27 22:04:00 +0000

    deploy: dbbe19013137e39d662a738ed22cedb47670774d

---
+++ _nuxt/static/1666908239/blog/first-post/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1666908239",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Ffirst-post",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"021e2bda"}}}}(null));
