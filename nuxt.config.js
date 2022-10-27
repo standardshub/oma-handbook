166f9738a33f66628bdbe095c3177a4c50740451
Joaquin <jprado@standardshub.io>
2022-10-27 14:27:11 -0700

    add n@nuxt/content and content directives

--- nuxt.config.js
+++ nuxt.config.js
@@ -38,8 +38,11 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    '@nuxt/content'
   ],
-
+  content: {
+    // Options
+  },
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
   }
